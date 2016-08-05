ca3d47d8ebca4ffa3ee724eaa350e617be82495a
Cakerr <ari.vuolas@cs.helsinki.fi>
2016-08-05 11:08:19 +0300

    Load all modules in ./features automatically

--- features/index.js
+++ features/index.js
@@ -1,14 +1,16 @@
-var features = [];
 var commands = {};
-var moro = require("./moro");
-features.push(moro);
 
-for (var i = 0; i < features.length; i++) {
-    Object.keys(features[i].commands).forEach(function (key, index) {
-        commands[key] = features[i].commands[key];
-    });
-}
-
+require("fs").readdirSync("./features/").forEach(function (file) {
+    //Exclude this file 
+    if (file !== "index.js") {
+        var req = require("./" + file);   
+        for (var command in req.commands){
+            commands[command] = req.commands[command];
+            console.log("Loaded command: " +command);
+        }
+    }
+});
+//TODO Find better place for this
 commands["!help"] = function (bot, from, to, message) {
     var commandsString = "Available commands: ";
     Object.keys(commands).forEach(function (key, index) {
